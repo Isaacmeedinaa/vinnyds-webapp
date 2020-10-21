2121ba1e310a65793a150d6f1589c605ff3868fb
Isaac Medina <isaac.meedinaa@gmail.com>
2020-10-21 13:00:44 -0500

    finished app configuration

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,35 @@
-var express = require('express');
-var router = express.Router();
+const express = require("express");
+const router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.sendFile("public/index.html", {
+    root: "./",
+  });
+});
+
+router.get("/index.html", function (req, res, next) {
+  res.sendFile("public/index.html", {
+    root: "./",
+  });
+});
+
+router.get("/about.html", function (req, res, next) {
+  res.sendFile("public/about.html", {
+    root: "./",
+  });
+});
+
+router.get("/our-menu.html", function (req, res, next) {
+  res.sendFile("public/our-menu.html", {
+    root: "./",
+  });
+});
+
+router.get("/contact-us.html", function (req, res, next) {
+  res.sendFile("public/contact-us.hmtl", {
+    root: "./",
+  });
 });
 
 module.exports = router;
